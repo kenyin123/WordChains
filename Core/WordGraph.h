e7fea748821dc560ddadb46acea02c75d971c3bb
16061190 <1664334639@qq.com>
2019-06-02 18:03:04 +0800

    gen_chain_char

--- Core/WordGraph.h
+++ Core/WordGraph.h
@@ -13,59 +13,60 @@
 
 struct WordListOptions
 {
-    bool countWord;
-    bool allowLoop;
-    char headChar;
-    char tailChar;
-    std::string fileName;
+	bool countWord;
+	bool allowLoop;
+	char headChar;
+	char tailChar;
+	std::string fileName;
 
-    WordListOptions()
-    {
-        countWord = true;
-        allowLoop = false;
-        headChar = '*';
-        tailChar = '*';
-        fileName = "";
-    }
+	WordListOptions()
+	{
+		countWord = true;
+		allowLoop = false;
+		headChar = '*';
+		tailChar = '*';
+		fileName = "";
+	}
 
-    void dump()
-    {
-        std::cout << "Count by " << (countWord ? "words" : "characters") << std::endl;
-        if (headChar != '*')
-        {
-            std::cout << "Head char: " << headChar << std::endl;
-        }
-        if (tailChar != '*')
-        {
-            std::cout << "Tail char: " << tailChar << std::endl;
-        }
-        std::cout << "File name: " << fileName << std::endl;
-    }
+	void dump()
+	{
+		std::cout << "Count by " << (countWord ? "words" : "characters") << std::endl;
+		if (headChar != '*')
+		{
+			std::cout << "Head char: " << headChar << std::endl;
+		}
+		if (tailChar != '*')
+		{
+			std::cout << "Tail char: " << tailChar << std::endl;
+		}
+		std::cout << "File name: " << fileName << std::endl;
+	}
 };
 
 class Core
 {
 private:
-    std::vector<std::string> _words;
-    std::unordered_map<char, std::vector<std::string> > _connections;
-    std::unordered_set<std::string> _sources;
-    WordListOptions _options;
-    int _maxLen;
-    std::vector<std::string> _maxPath;
+	std::vector<std::string> _words;
+	std::unordered_map<char, std::vector<std::string> > _connections;
+	std::unordered_set<std::string> _sources;
+	WordListOptions _options;
+	int _maxLen;
+	std::vector<std::string> _maxPath;
 
-    void addWord(const std::string& word);
-    bool search(std::vector<std::string> &result);
-    void searchWorker(std::vector<std::string> &path, int c_word, int c_char);
-    Core() : _maxLen(0) {}
+	void addWord(const std::string& word);
+	bool search(std::vector<std::string> &result);
+	void searchWorker(std::vector<std::string> &path, int c_word, int c_char);
+	Core() : _maxLen(0) {}
 
 public:
-    ~Core() {}
+	~Core() {}
 
-    std::vector<std::string> go(const std::vector<std::string>& words, const WordListOptions& op);
-    void dumpWords();
-    
-    CORE_API static int gen_chain_word(char* words[], int len, char* result[], char head, char tail, bool enable_loop);
-    CORE_API static std::vector<std::string> read_file(const std::string& file_name);
+	std::vector<std::string> go(const std::vector<std::string>& words, const WordListOptions& op);
+	void dumpWords();
+
+	CORE_API static int gen_chain_word(char* words[], int len, char* result[], char head, char tail, bool enable_loop);
+	CORE_API static int gen_chain_char(char* words[], int len, char* result[], char head, char tail, bool enable_loop);
+	CORE_API static std::vector<std::string> read_file(const std::string& file_name);
 };
 
 std::ostream& operator<<(std::ostream& out, const std::vector<std::string>& val);
